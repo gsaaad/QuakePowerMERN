f9705bd2db9de3c9e9de857373e141f6b6d8f101
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-01 18:31:10 -0400

    initial commit QuakePower MERN

---
+++ server/config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/quake-power", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
