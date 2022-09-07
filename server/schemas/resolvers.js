8b8a297f05efb4f9e8f29f8a290ab65dcc5d9a9f
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-07 18:08:30 -0400

    added ability to query earthquakes and users

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,10 +4,20 @@ const dateScalar = require("./dateScalar");
 const resolvers = {
   Query: {
     earthquakes: async (parent, { username }) => {
-      const params = username ? { username } : {};
-
       // return Earthquake.find().sort();
+      const params = username ? { username } : {};
       return Earthquake.find(params).sort();
+    },
+    earthquake: async (parent, { _id }) => {
+      return Earthquake.findOne({ _id });
+    },
+    users: async () => {
+      return User.find().select("-__v -password").populate("earthquakes");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("earthquakes");
     },
   },
   DateScalar: {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,7 +6,15 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   scalar DateScalar
 
+  type User {
+    _id: ID
+    username: String
+    email: String
+    earthquakes: [Earthquake]
+  }
+
   type Earthquake {
+    _id: ID
     username: String
     Date: DateScalar
     Latitude: Float
@@ -15,10 +23,20 @@ const typeDefs = gql`
     Depth: Int
     Region: String
     reactionCount: Int
+    reactions: [Reaction]
+  }
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
   }
 
   type Query {
+    users: [User]
+    user(username: String!): User
     earthquakes(username: String): [Earthquake]
+    earthquake(_id: ID!): Earthquake
   }
 `;
 
