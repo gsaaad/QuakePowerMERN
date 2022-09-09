31d2e0785e6753754eff2d789e5ee18fed5bb706
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-09 07:45:44 -0400

    updated files and added concunrrently to merge and start server and react client together

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
-
+const path = require("path");
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const dbConnection = require("./config/connection");
@@ -27,6 +27,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // integrate our apollo server with the express application as middleware
   server.applyMiddleware({ app });
 };
+
+// server up statis assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 dbConnection.on("error", console.error.bind(console, "connection error!"));
 dbConnection.once("open", () => {
