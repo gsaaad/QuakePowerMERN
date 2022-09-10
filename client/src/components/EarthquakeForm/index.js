f0ddfcfa85b8eba139dc8682459dfcff2e5811e8
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-10 10:04:20 -0400

    added ability for user to utilize earthquakeForm in frontend to add an earthquake to database, next set is adding to mutation in backend to receive that data

---
+++ client/src/components/EarthquakeForm/index.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+
+const EarthquakeForm = () => {
+  const [formState, setFormState] = useState({
+    Date: "",
+    Latitude: "",
+    Longitude: "",
+    Depth: "",
+    Magnitude: "",
+    Region: "",
+  });
+
+  //   update state based on form input to add earthquake
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormState({
+      // keep pre-existing values in form, and setFormState
+      ...formState,
+      [name]: value,
+    });
+  };
+  //   handleForm
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  };
+
+  return (
+    <div>
+      <form className="form-earthquake" onSubmit={handleFormSubmit}>
+        <label htmlFor="date-input" className="form-label">
+          Date
+        </label>
+        <input
+          name="Date"
+          id="date-input"
+          value={formState.Date}
+          className="form-input"
+          type="date"
+          onChange={handleChange}
+        />
+
+        <p>Latitude</p>
+        <input
+          name="Latitude"
+          value={formState.Latitude}
+          className="form-input"
+          type="string"
+          onChange={handleChange}
+        />
+        <p>Longitude</p>
+        <input
+          name="Longitude"
+          value={formState.Longitude}
+          className="form-input"
+          type="string"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="depth-input" className="form-label">
+          Depth
+        </label>
+        <input
+          name="Depth"
+          className="form-input"
+          type="string"
+          value={formState.Depth}
+          onChange={handleChange}
+        />
+        <label htmlFor="magnitude-input" className="form-label">
+          Magnitude
+        </label>
+        <input
+          name="Magnitude"
+          className="form-input"
+          type="string"
+          value={formState.Magnitude}
+          onChange={handleChange}
+        />
+        <label htmlFor="region-input" className="form-label">
+          Region
+        </label>
+        <input
+          name="Region"
+          id="region-input"
+          className="form-input"
+          type="string"
+          value={formState.Region}
+          onChange={handleChange}
+        />
+        <button className="btn">Add Earthquake</button>
+      </form>
+    </div>
+  );
+};
+
+export default EarthquakeForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import EarthquakeList from "../components/EarthquakeList";
-
 import { useQuery } from "@apollo/client";
 import { QUERY_EARTHQUAKES } from "../utils/queries";
+import Auth from "../utils/authenticate";
+import EarthquakeForm from "../components/EarthquakeForm";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_EARTHQUAKES);
@@ -11,8 +12,19 @@ const Home = () => {
   const earthquakes = data?.earthquakes || [];
   console.log(earthquakes);
 
+  const loggedIn = Auth.loggedIn();
+
   return (
     <main>
+      <div className="flex-row justify-space-between">
+        {/* if logged in, you can add earthquake! */}
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <EarthquakeForm />
+          </div>
+        )}
+        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}></div>
+      </div>
       <div className="flex-row justify-space-between">
         {/*TODO get a live feed component going */}
         {/* this line is if you want to conditional render, and add another column to the right for live feed/ */}
