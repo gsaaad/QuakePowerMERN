179c6067f90566a81669ba88eb7b76d9e46ed78b
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-19 12:26:35 -0400

    added largest page to header to allow user navigation using REACT Route

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -24,6 +24,7 @@ const Header = () => {
                 Logout
               </a>
               <Link to="/deadliestearthquakes">Deadliest</Link>
+              <Link to="/largestearthquakes">Largest</Link>
             </>
           ) : (
             <>
