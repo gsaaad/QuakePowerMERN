afe1e93ac6619cd1da0fa49d11e6d49cbe8fb097
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-09-19 06:10:01 -0400

    added 1 post for earthquake

--- client/src/pages/Deadliest.js
+++ client/src/pages/Deadliest.js
@@ -2,34 +2,26 @@ import React from "react";
 import Auth from "../utils/authenticate";
 import Earthquakes from "../data/deadliest.json";
 import $ from "jquery";
+const displayArea = $("#display-area");
 const Deadliest = () => {
   const loggedIn = Auth.loggedIn();
-  const displayArea = $("#display-area");
-  displayArea.text("Hello!");
-  console.log(Earthquakes.deadliest);
 
-  const printResults = (id, Date, Year, Depth, Location, Notes, Magnitude) => {
-    const earthquakeDisplay = ` <div class="col-12 col-md-5 mb-3">
-        <div class="card p-3" data-id=${id}>
-        <h4 class="text-dark">${Location}</h4>
-        <p>ID:${id}</p>
-        <p>Date: ${Date} ${Year}<br/>
-           Depth: ${Depth}<br/>
-           Magnitude: ${Magnitude}<br/>
-           Notes: ${Notes}
-          </p>
-        </div>
-        </div>`;
+  Object.entries(Earthquakes.deadliest).forEach(([key, earthquake]) => {
+    console.log("this is each earthquake", key, earthquake);
+    const earthquakeDisplay = `<div class="col-12 col-md-5 mb-3">
+             <div class="card p-3" data-id=${earthquake.id}>
+             <h4 class="text-dark">${earthquake.Location}</h4>
+             <p>ID:${earthquake.id}</p>
+             <p>Date: ${earthquake.Date} ${earthquake.Year}<br/>
+                Depth: ${earthquake.Depth}<br/>
+                Magnitude: ${earthquake.Magnitude}<br/>
+                Notes: ${earthquake.Notes}
+               </p>
+             </div>
+           </div>`;
     displayArea.html(earthquakeDisplay);
     return earthquakeDisplay;
-  };
-
-  Object.entries(Earthquakes.deadliest).forEach(
-    ([key, { id, Date, Year, Depth, Location, Notes, Magnitude }]) => {
-      console.log(key);
-      printResults(id, Date, Year, Depth, Location, Notes, Magnitude);
-    }
-  );
+  });
 
   //   const printResults = (resultArr) => {
   //     console.log(resultArr);
@@ -55,7 +47,8 @@ const Deadliest = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}></div>
+        <div id="display-area"></div>
+        {/* <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}></div> */}
       </div>
       <div className="flex-row justify-space-between">
         {/*TODO get a live feed component going */}
